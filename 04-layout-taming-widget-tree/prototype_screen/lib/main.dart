946d7bf0d6384ef23f171246b1d79eb19703f2ae
Miguel <hrypasato@gmail.com>
2022-01-16 17:31:12 -0500

    create deeptree class

--- 04-layout-taming-widget-tree/prototype_screen/lib/main.dart
+++ 04-layout-taming-widget-tree/prototype_screen/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prototype_screen/deep_tree.dart';
 import 'package:prototype_screen/flex_screen.dart';
 import 'package:prototype_screen/profile_screen.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ProfileScreen(),
+      home: DeepTree(),
     );
   }
 }
