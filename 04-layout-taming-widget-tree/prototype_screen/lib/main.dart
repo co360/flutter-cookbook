1e168114f02d9b435c615c3ecb8698ea3315747d
Miguel <hrypasato@gmail.com>
2022-01-16 16:03:15 -0500

    app display FlexScreen

--- 04-layout-taming-widget-tree/prototype_screen/lib/main.dart
+++ 04-layout-taming-widget-tree/prototype_screen/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prototype_screen/flex_screen.dart';
 import 'package:prototype_screen/profile_screen.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ProfileScreen(),
+      home: FlexScreen(),
     );
   }
 }
