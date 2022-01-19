94f5d512c7272a81e9a6e89e59ec8646285b5065
Miguel <hrypasato@gmail.com>
2022-01-18 22:10:12 -0500

    create stopwatch widget

---
+++ 05-adding-interactivity/stopwatch/lib/stopwatch.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class StopWatch extends StatefulWidget {
+  const StopWatch({Key? key}) : super(key: key);
+
+  @override
+  _StopWatchState createState() => _StopWatchState();
+}
+
+class _StopWatchState extends State<StopWatch> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stopwatch'),
+      ),
+      body: Center(
+        child: Text(
+          '0 seconds',
+          style: Theme.of(context).textTheme.headline5,
+        ),
+      ),
+    );
+  }
+}
