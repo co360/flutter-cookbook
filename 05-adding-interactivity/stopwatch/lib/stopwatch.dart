f04b4ecf7088bf51f12526bbbfc792fae3f3fb03
Miguel <hrypasato@gmail.com>
2022-01-20 11:46:42 -0500

    add start and stop buttons

--- 05-adding-interactivity/stopwatch/lib/stopwatch.dart
+++ 05-adding-interactivity/stopwatch/lib/stopwatch.dart
@@ -9,18 +9,86 @@ class StopWatch extends StatefulWidget {
 }
 
 class _StopWatchState extends State<StopWatch> {
+  int seconds = 0;
+  late Timer timer;
+  late bool isTicking = false;
+
+  void _onTick(Timer time) {
+    setState(() {
+      ++seconds;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Stopwatch'),
       ),
-      body: Center(
-        child: Text(
-          '0 seconds',
-          style: Theme.of(context).textTheme.headline5,
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            '$seconds ${_secondsText()}',
+            style: Theme.of(context).textTheme.headline5,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                ),
+                child: Text('Start'),
+                onPressed: isTicking ? null : _startTimer,
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                ),
+                onPressed: isTicking ? _stopTimer : null,
+                child: Text('Stop'),
+              ),
+            ],
+          )
+        ],
       ),
     );
+  }
+
+  String _secondsText() => seconds == 1 ? 'second' : 'seconds';
+
+  void _startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), _onTick);
+
+    setState(() {
+      seconds = 0;
+      isTicking = true;
+    });
+  }
+
+  void _stopTimer() {
+    timer.cancel();
+
+    setState(() {
+      isTicking = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
   }
 }
