bf5a4421c0229e3a6598c0a79e58c188efc1e7b1
Miguel <hrypasato@gmail.com>
2022-01-23 10:18:07 -0500

    add scrolling viewport

--- 05-adding-interactivity/stopwatch/lib/stopwatch.dart
+++ 05-adding-interactivity/stopwatch/lib/stopwatch.dart
@@ -13,6 +13,8 @@ class _StopWatchState extends State<StopWatch> {
   late Timer timer;
   late bool isTicking = false;
   final laps = <int>[];
+  final itemHeight = 60.0;
+  final scrollController = ScrollController();
 
   void _onTick(Timer time) {
     setState(() {
@@ -128,17 +130,26 @@ class _StopWatchState extends State<StopWatch> {
       laps.add(milliseconds);
       milliseconds = 0;
     });
+
+    scrollController.animateTo(itemHeight * laps.length,
+        duration: Duration(milliseconds: 500), curve: Curves.easeIn);
   }
 
   Widget _buildLapDisplay() {
-    return ListView(
-      children: [
-        for (int milliseconds in laps)
-          ListTile(
-            title: Text(_secondsText(milliseconds)),
-          )
-      ],
-    );
+    return Scrollbar(
+        child: ListView.builder(
+      controller: scrollController,
+      itemExtent: itemHeight,
+      itemCount: laps.length,
+      itemBuilder: (context, index) {
+        final milliseconds = laps[index];
+        return ListTile(
+          contentPadding: EdgeInsets.symmetric(horizontal: 50),
+          title: Text('Lap ${index + 1}'),
+          trailing: Text(_secondsText(milliseconds)),
+        );
+      },
+    ));
   }
 
   @override
